ab8037fffb23fdb20f20e5447d75920e0bcb538b
mayooghaml <mayooghamuraleedharan@gmail.com>
2022-02-10 10:35:16 +0530

    new

---
+++ backend/src/model/BlogDB.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+//mongoose.connect("mongodb://localhost:27017/my-blog");
+mongoose.connect("mongodb+srv://userone:userone@ictakfiles.dunq6.mongodb.net/user?retryWrites=true&w=majority");
+const Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+    title: String,
+    description: String,
+    name: String,
+    upvoters: Array,
+    comments: Array
+})
+
+var ArticleInfo = mongoose.model("articles", articleSchema);
+
+module.exports = ArticleInfo;
---
+++ frontendd/src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Home</h1>
+            <br></br>
+        <h3><p>This is the Home Page</p></h3>
+        </div>
+    );
+};
+
+export default Home;
